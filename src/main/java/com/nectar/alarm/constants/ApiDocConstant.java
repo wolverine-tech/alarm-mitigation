ce23ba5a27e7d1cee9e97fd07343c269f2e2a27f
Wolverine <manu@nectarit.com>
2021-11-18 12:42:04 +0530

    adding constants

---
+++ src/main/java/com/nectar/alarm/constants/ApiDocConstant.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Nectar IT
+ *
+ * Contributors:
+ *
+ * 1. natasharomanoff
+ * 2. wolverine-tech
+ *
+ * Since Oct 2021
+ *******************************************************************************/
+
+package com.nectar.alarm.constants;
+
+public interface ApiDocConstant {
+
+	public static final String GENERAL_FIELD_NOT_SPECIFIED_CODE = "505";
+	public static final String GENERAL_SUCCESS_RESP_CODE = "200";
+	public static final String GENERAL_DATA_NOT_AVAILABLE_CODE = "500";
+	public static final String GENERAL_FIELD_INVALID_CODE = "508";
+	public static final String PERSISTENCE_ERROR_CODE = "503";
+	public static final String GENERAL_FIELD_NOT_UNIQUE_CODE = "522";
+
+	public static final String GENERAL_FIELD_NOT_SPECIFIED = "{mandatory-field} not specified";
+	public static final String GENERAL_FIELD_INVALID = "{field} is invalid";
+	public static final String GENERAL_DATA_NOT_AVAILABLE = "Requested data is not available";
+	public static final String PERSISTENCE_ERROR = "Data could not be saved";
+	public static final String GENERAL_FIELD_NOT_UNIQUE = "{field} is not unique";
+	
+	
+
+
+
+
+}

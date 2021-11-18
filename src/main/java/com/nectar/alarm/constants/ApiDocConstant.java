dc126d8e6d7b622111348646b0d44dea4337a283
Wolverine <manu@nectarit.com>
2021-11-18 12:45:31 +0530

    Update ApiDocConstant.java

--- src/main/java/com/nectar/alarm/constants/ApiDocConstant.java
+++ src/main/java/com/nectar/alarm/constants/ApiDocConstant.java
@@ -26,7 +26,24 @@ public interface ApiDocConstant {
 	public static final String PERSISTENCE_ERROR = "Data could not be saved";
 	public static final String GENERAL_FIELD_NOT_UNIQUE = "{field} is not unique";
 	
-	
+	public static final String LIST_ALARM_MITIGATION_CONFIG_WITH_PAGINATION_SUMMARY = "API for list alarm mitigation with pagination";
+	public static final String LIST_ALARM_MITIGATION_CONFIG_WITH_PAGINATION_DESC = "This API is used for listing alarm mitigation with pagination and also supports searching and sorting with multiple fields";
+
+	public static final String CREATE_ALARM_MITIGATION_CONFIG_SUMMARY = "API for creating alarm mitigation";
+	public static final String CREATE_ALARM_MITIGATION_CONFIG_DESC = "This API is used for creating alarm mitigation";
+
+	public static final String UPDATE_ALARM_MITIGATION_CONFIG_SUMMARY = "API for updating alarm mitigation";
+	public static final String UPDATE_ALARM_MITIGATION_CONFIG_DESC = "This API is used for update alarm mitigation with given Id";
+
+
+	public static final String DELETE_ALARM_MITIGATION_CONFIG_SUMMARY = "API for deleting alarm mitigation";
+	public static final String DELETE_ALARM_MITIGATION_CONFIG_DESC = "This API is used for deleting alarm mitigation with given Id";
+
+	public static final String FIND_ALARM_MITIGATION_CONFIG_SUMMARY = "API for finding alarm mitigation";
+	public static final String FIND_ALARM_MITIGATION_CONFIG_DESC = "This API is used for finding alarm mitigation for given Id";
+
+	public static final String LIST_ALARM_MITIGATION_CONFIG_WITH_DOMAIN_SUMMARY = "API for listing alarm mitigation with domain";
+	public static final String LIST_ALARM_MITIGATION_CONFIG_WITH_DOMAIN_DESC = "This API is used for listing alarm mitigation with given domain";
 
 
 
